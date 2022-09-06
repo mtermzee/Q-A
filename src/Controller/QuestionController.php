28dcb2aa9ad9aff7416dd194c7518c153754c2a3
mtermzee <m.termzee@gmail.com>
2022-09-06 16:16:25 +0200

    add KnpMarkdownBundle

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -5,6 +5,7 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Knp\Bundle\MarkdownBundle\MarkdownParserInterface;
 
 class QuestionController extends AbstractController
 {
@@ -17,18 +18,22 @@ class QuestionController extends AbstractController
     }
 
     #[Route('/question/{slug}', name: 'app_question_show')]
-    public function show($slug): Response
+    public function show($slug, MarkdownParserInterface $parser): Response
     {
         $answers = [
-            'This is my first answer',
+            'This is my `first` answer',
             'This is my second answer',
             'This is my third answer',
         ];
+        $questionText = 'Why does the sun go on shining?';
+        $parsedQuestionText = $parser->transformMarkdown($questionText);
+
         // debuging
         dump($slug, $this);
 
         return $this->render('question/show.html.twig', [
             'question' => ucwords(str_replace('-', ' ', $slug)),
+            'questionText' => $parsedQuestionText,
             'answers' => $answers,
         ]);
     }
