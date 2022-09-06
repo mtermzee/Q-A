6e13e59f4a7c522af95586c8ba226962ec26ea99
mtermzee <m.termzee@gmail.com>
2022-09-06 15:27:07 +0200

    add logger interface

--- src/Controller/CommentController.php
+++ src/Controller/CommentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use Psr\Log\LoggerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,14 +20,16 @@ class CommentController extends AbstractController
 
     //commentVote
     #[Route('/comments/{id}/vote/{direction<up|down>}', name: 'app_comment_vote', methods: ['POST'])]
-    public function commentVote($id, $direction): Response
+    public function commentVote($id, $direction, LoggerInterface $logger): Response
     {
         // todo use id to query database for comment
 
         // use real vote logic to save this to the database
         if ($direction === 'up') {
+            $logger->info('Voting up!');
             $currentVoteCount = rand(7, 100);
         } else {
+            $logger->info('Voting down!');
             $currentVoteCount = rand(0, 5);
         }
 
