6c0fbb5d00b0ef6863a705ae073e28c42bb747a5
mtermzee <m.termzee@gmail.com>
2022-09-08 14:06:55 +0200

    Entity objects in Twig

--- src/Controller/QuestionController.php
+++ src/Controller/QuestionController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Question;
 use App\Service\MarkdownHelper;
+use Doctrine\ORM\EntityManager;
+use Doctrine\ORM\EntityManagerInterface;
+use Doctrine\ORM\Mapping\Entity;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,7 +15,7 @@ use Symfony\Contracts\Cache\CacheInterface;
 
 class QuestionController extends AbstractController
 {
-    #[Route('/question', name: 'app_homepage')]
+    #[Route('/questions', name: 'app_homepage')]
     public function index(): Response
     {
         return $this->render('question/index.html.twig', [
@@ -19,29 +23,64 @@ class QuestionController extends AbstractController
         ]);
     }
 
-    #[Route('/question/{slug}', name: 'app_question_show')]
-    public function show($slug, MarkdownParserInterface $parser, CacheInterface $cache, MarkdownHelper $markdownHelper): Response
+    #[Route('/questions/new')]
+    public function new(EntityManagerInterface $em): Response
     {
+        $question = new Question();
+        $question->setName('Some question')
+            ->setSlug('some-question-' . rand(0, 1000))
+            ->setQuestion(<<<EOF
+                This is a question to test how it works.
+              EOF);
+        if (rand(1, 10) > 2) {
+            $question->setAskedAt(new \DateTime(sprintf('-%d days', rand(1, 100))));
+        }
+
+        //dd($question);
+        // add to database
+        $em->persist($question);
+        $em->flush();
+
+        return new Response(sprintf(
+            'Hiya! We just created a new question with the id: %s',
+            $question->getId(),
+            $question->getSlug()
+        ));
+    }
+
+    #[Route('/questions/{slug}', name: 'app_question_show')]
+    public function show($slug, EntityManagerInterface $em, MarkdownParserInterface $parser, CacheInterface $cache, MarkdownHelper $markdownHelper): Response
+    {
+        // get Data from database
+        $repository = $em->getRepository(Question::class);
+        /**  @var Qeustion|null $question */
+        $question = $repository->findOneBy(['slug' => $slug]);
+        if (!$question) {
+            throw $this->createNotFoundException(sprintf('No question found for slug "%s"', $slug));
+        }
+
         $answers = [
             'This is my `first` answer',
             'This is my second answer',
             'This is my third answer',
         ];
-        $questionText = 'Why does the *sun* go on shining?';
+
         //KnpMarkdownBundle
         //$parsedQuestionText = $parser->transformMarkdown($questionText);
         // cache
         /*$parsedQuestionText = $cache->get('markdown_' . md5($questionText), function () use ($questionText, $parser) {
             return $parser->transformMarkdown($questionText);
         }); now with the new own Serivce */
-        $parsedQuestionText = $markdownHelper->parse($questionText, $parser, $cache);
+        /*$questionText = 'Why does the *sun* go on shining?';
+        $parsedQuestionText = $markdownHelper->parse($questionText, $parser, $cache);*/
 
         // debuging
         dump($slug, $this);
 
         return $this->render('question/show.html.twig', [
-            'question' => ucwords(str_replace('-', ' ', $slug)),
-            'questionText' => $parsedQuestionText,
+            /*'question' => ucwords(str_replace('-', ' ', $slug)),
+            'questionText' => $parsedQuestionText,*/
+            'question' => $question,
             'answers' => $answers,
         ]);
     }
