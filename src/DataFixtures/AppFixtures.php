ebd1117b1cead7feda54fe1d4cdec07d5dabe508
mtermzee <m.termzee@gmail.com>
2022-09-13 09:40:17 +0200

    Saving Items in a ManyToMany Relation

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -4,6 +4,7 @@ namespace App\DataFixtures;
 
 use App\Entity\Answer;
 use App\Entity\Question;
+use App\Entity\Tag;
 use App\Factory\AnswerFactory;
 use App\Factory\QuestionFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
@@ -66,6 +67,18 @@ class AppFixtures extends Fixture
             ];
         });
 
+        $question = QuestionFactory::createOne()->object();
+        $tag1 = new Tag();
+        $tag1->setName('diosaurs');
+        $tag2 = new Tag();
+        $tag2->setName('Monster trucks');
+
+        // realte tgas to questions
+        $question->addTag($tag1);
+        $question->addTag($tag2);
+
+        $manager->persist($tag1);
+        $manager->persist($tag2);
 
         $manager->flush();
     }
